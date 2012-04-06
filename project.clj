629b008dc2c61b91dbd1c67327c0fe16e6cfc51d
pgdad <pgdad@hotmail.com>
2012-04-06 14:38:48 -0400

    wif

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-zoo "1.0.0-SNAPSHOT"
+(defproject clj-zoo "1.0.1"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"] [zookeeper-clj "0.9.2"]]
-  :repl-init clj-zoo.core
+  :repl-init clj-zoo.serverSession
   :aot [clj-zoo.util clj-zoo.server clj-zoo.client clj-zoo.core
-	clj-zoo.servicewatcher])
+	clj-zoo.servicewatcher clj-zoo.serverSession])
